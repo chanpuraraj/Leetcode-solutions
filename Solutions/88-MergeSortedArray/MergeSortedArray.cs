af12ca4369ffea71598ba61ec69c1b6b5ebdd956
Raj Chanpura <raj.chanpura@dayforce.com>
2025-10-23 21:42:38 -0400

    Added Merged Sorted Array solution and problem statement

---
+++ Solutions/88-MergeSortedArray/MergeSortedArray.cs
@@ -0,0 +1,12 @@
+
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+		for (int i = 0; i < n; i++)
+		{
+			nums1[m+i] = nums2[i];
+			//Console.WriteLine("nums2[i-n]"+ nums2[i - n]);
+			Console.WriteLine("nums1[i]",+nums1[i]);
+		}
+		Array.Sort(nums1);
+	}
+}
